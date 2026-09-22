1f1a655edc80e61be026ea47a81e0edbcf24de7b
Vineet Bambah <neozvbambah@gmail.com>
2026-09-23 01:46:35 +0530

    self implementation of map

--- log-analyzer/lab5.cpp
+++ log-analyzer/lab5.cpp
@@ -7,7 +7,11 @@ struct logLine{
     string action;
 };
 int main(){
+    int numOfEvents=0;
+    vector<string> users;
+    bool userFound;
     vector<logLine> logEntries;
+    int maxIndex=0;
     logLine tempInp;
     do{
         cin>>tempInp.date;
@@ -16,10 +20,36 @@ int main(){
             logEntries.push_back(tempInp);
         }
     }while(tempInp.date!="END");
-    for (auto log : logEntries) {
-        cout << log.date << " "
-             << log.timestamp << " "
-             << log.username << " "
-             << log.action << '\n';
+    for(auto log:logEntries){
+        numOfEvents++;
     }
+    for(int i=0;i<logEntries.size();i++){
+        userFound=false;
+        for(int j=0;j<users.size();j++){
+            if(users[j]==logEntries[i].username){
+                userFound=true;
+                break;
+            }
+        }
+       if(!userFound){
+           users.push_back(logEntries[i].username);
+       }
+    }
+    vector<int>freq(users.size(),0);
+    for(int i=0;i<logEntries.size();i++){
+        auto it=find(users.begin(),users.end(),logEntries[i].username);
+        auto index=it-users.begin();
+        freq[index]++;
+    }
+    for(int i=1;i<freq.size();i++){
+        if(freq[i]>freq[maxIndex]){
+            maxIndex=i;
+        }
+    }
+    cout<<"Total number of events: "<<numOfEvents<<endl;
+    cout<<"Number of unique users: "<<users.size()<<endl;
+    for(int i=0;i<users.size();i++){
+        cout<<users[i]<<":"<<freq[i]<<endl;
+    }
+    cout<<"Most active user: "<<users[maxIndex]<<endl;
 }
